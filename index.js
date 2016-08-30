f542883df35d91d239fe26d78fbf7d45760eed36
vklymkiv <volodymyr.klymkiv@gmail.com>
2016-08-30 12:32:03 +0000

    a

---
+++ index.js
@@ -0,0 +1,42 @@
+// var hello='Hello World';
+// document.write(hello);
+// console.log(hello);
+// alert(hello);
+
+// var num = 10;
+// var bool = true;
+// var sum = 10 + true;
+// document.write(sum);
+
+//  var num = 10;
+//  var str = '10';
+//  var sum = num + str;
+//  document.write(sum);
+ 
+ function sum(a,b) {
+     var result = a + b;
+     return result;
+}
+
+function lf(a, b) {
+    if (a > b) {
+        return a;
+    }   else {
+        return b;
+    }
+}
+
+// var res = sum(2, 2);
+// document.write(res);
+// document.write('<br>');
+// document.write(sum(5, 10));
+// document.write('<br>');
+// document.write(sum(num, 15));
+// document.write('<br>');
+
+document.write(lg(10, 20));
+document.write('<br>');
+document.write(lg(1025643, 5354654));
+document.write('<br>');
+
+document.write(Math.sqrt(16));
---
+++ index1.js
@@ -0,0 +1,31 @@
+// var a = 1, b = 1, c, d;
+
+// c = ++a; alert(c); // 2
+// d = b++; alert(d); // 1
+
+// c = (2+ ++a); alert(c); // 5
+// d = (2+ b++); alert(d); // 4
+
+// alert(a); // 3
+// alert(b); // 3
+
+function squareEquation() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var c = document.getElementById('c').value;
+    var sol = document.getElementById('squareSol');
+    
+    var d = b * b - 4 * a * c;
+    if ( d > 0 ) {
+    str += (-b + math.sqrt(d)) / (2 * a);
+    str += 
+    str += (-b - math.sqrt(d)) / (2 * a);
+  } else {
+      if ( d = 0) {
+      str += -b / ( 2 * a );
+  } else {
+      str += -b / ( 2 * a );
+      str +=  Math.sqrt( -d ) / ( 2 * a );
+  }
+}
+    
